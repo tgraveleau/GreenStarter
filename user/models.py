90a2c70c9d39c585ea106d988f5839def1e24c6b
tgraveleau <teddy.graveleau@orange.fr>
2019-01-21 17:24:31 +0100

    Karma plus model

--- user/models.py
+++ user/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Karma(models.Model):
-    valeur = models.IntegerField(default=0)
-
-    def __str__(self):
-        return str(self.id)
-
 class User(models.Model):
     class Meta:
         ordering = ['first_name', 'last_name']
@@ -16,7 +10,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=1)
-    karma = models.ForeignKey(Karma, on_delete=models.CASCADE)
+    karma = models.IntegerField(default=0)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name + ' (' + str(self.username) + ')'
